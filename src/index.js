dfecf9e60da802b83c4d281a69c70a937733a9dc
prudence <prudence.jerono@student.moringaschool.com>
2023-04-23 12:37:14 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const props = {
+  title: "My App Title"
+};
+
+ReactDOM.render(<App {...props} />, document.getElementById("root"));
+
+import React from "react";
+
+function App({title}) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>Welcome to my app</p>
+    </div>
+  );
+}
+
+export default App;
